37d16a4f019cc4d73b9620b06bfe088bb172005f
WalJT <waljt@pm.me>
2023-10-30 13:52:39 +0000

    Updated script for Miniconda installation

--- install_software.sh
+++ install_software.sh
@@ -23,7 +23,9 @@ rm ./google-chrome-stable_current_amd64.deb
 sudo snap install code --classic
 
 # Miniconda
-# https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash ./Miniconda3-latest-Linux-x86_64.sh
+rm ./Miniconda3-latest-Linux-x86_64.sh
 
 # LaTeX + Kile
 sudo apt install texlive-{latex,fonts}-{recommended,extra} biber texlive-science kile
