be5034291b9f965251a62468a209c317d7c53cd3
WalJT <waljt@pm.me>
2023-11-05 17:09:44 +0000

    Add todo list

---
+++ TODO.md
@@ -0,0 +1,14 @@
+# TODO
+
+- [ ] Steam setup
+  - [ ] `sudo-dpkg --add-architecture i386`
+- [ ] Plymouth (fancy boot screen) (<https://wiki.debian.org/plymouth>)
+- [ ] Virtualisation Setup (quemu / KVM) (<https://wiki.debian.org/KVM>)
+- [ ] Git Configuration
+- [ ] Snap Packages
+  - [ ] Visual Studio Code
+    - That's actually it. Maybe I should use the `.deb` or `.tar.gz` instead
+- [ ] Photography sofrware
+  - [ ] Rapid Photo Downloader (deb)
+  - [ ] Rawtherapee (Flatpak)
+  - [ ] Digikam (Flatpak)
--- install_software.sh
+++ install_software.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 # Install applications that I use on Debian.
-# Assumes that your sources.list file has been configured
-# and your system is up-to-date
+# Assumes that your sources.list file has been configured,
+# Flatpak & Flathub have been set up, and your system is up-to-date
 
-# Work in progress. Does not account for snap and flatpak setup
 
 # Refresh the repositories
 sudo apt update
 
 # Firefox
-sudo snap install firefox
+sudo apt remove firefox-esr --auto-remove --purge
+flatpak install flathub app/org.mozilla.firefox/x86_64/stable
 
 # Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 rm ./google-chrome-stable_current_amd64.deb
-
-# Git
-# sudo apt install git
-
-# VSCode
-sudo snap install code --classic
 
 # Miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
